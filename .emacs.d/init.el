3f980b0107f376ec5556e82ce27bd0142839d865
Joshua Wolfe <joshuafwolfe@gmail.com>
2016-04-30 23:20:42 -0400

    Added emacs comfig

---
+++ .emacs.d/init.el
@@ -0,0 +1,109 @@
+;; __        __    _  __      _       _       _ _         _ 
+;; \ \      / /__ | |/ _| ___( )___  (_)_ __ (_) |_   ___| |
+;;  \ \ /\ / / _ \| | |_ / _ \// __| | | '_ \| | __| / _ \ |
+;;   \ V  V / (_) | |  _|  __/ \__ \ | | | | | | |_ |  __/ |
+;;    \_/\_/ \___/|_|_|  \___| |___/ |_|_| |_|_|\__(_)___|_|
+                                                          
+
+;; Setup package control
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Essential settings.
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(tool-bar-mode -1) ; No toolbar
+(scroll-bar-mode -1) ; Hide scrollbars
+(setq initial-scratch-message "") ; No scratch text
+(use-package sublime-themes
+  :ensure t
+  :config
+  (load-theme 'spolsky t)) ; Color theme
+
+;; Base evil package
+(use-package evil
+  :ensure t
+  :init
+  ;; Unbind <C-u> for evil mode's use
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode t)
+  ;; Move up and down through wrapped lines
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line))
+
+;; evil leader key
+(use-package evil-leader
+  :ensure t
+  :config
+  (evil-leader/set-leader "<SPC>")
+  (setq evil-leader/in-all-states 1)
+  (global-evil-leader-mode)
+  (evil-leader/set-key
+    "w"  'save-buffer
+    "so" 'eval-buffer
+    "bb" 'mode-line-other-buffer
+    "bn" 'next-buffer
+    "bp" 'previous-buffer
+    "bd" 'kill-buffer))
+
+;; Tpope's surround 
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+;; Narrowing completion engine
+(use-package helm
+  :ensure t
+  :config 
+  (global-set-key (kbd "M-x")     'undefined)
+  (global-set-key (kbd "M-x")     'helm-M-x)
+  (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x C-b") 'undefined)
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (helm-mode t))
+
+;; Backup options
+;; backup in one place. flat, no tree structure
+(setq backup-directory-alist '(("" . "~/.emacs.bak")))
+(setq backup-by-copying t) ; Stop shinanigans with links
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

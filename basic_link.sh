e1025a5e0b2dd51390cfc37565808fd47cd8f54a
Josh Wolfe <joshuafwolfe@gmail.com>
2016-10-31 10:45:50 -0400

    Added basic linking scripts

---
+++ basic_link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s ./.vim ~
+ln -s ./.vimrc ~
+ln -s ./.emacs.d ~
+ln -s ./.tmux.conf ~
+ln -s ./zsh ~
+ln -s ./zshrc ~
+ln -s ./Xmodmap ~
---
+++ full_link.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -s ./bspwm ~/.config
+ln -s ./.emacs.d ~
+ln -s ./sxhkd ~/.config
+ln -s ./.tmux.conf ~
+ln -s ./.vim ~
+ln -s ./.vimrc ~
+ln -s ./.Xmodmap ~
+ln -s ./.Xresources ~
+ln -s ./.zsh ~
+ln -s ./.zshrc ~

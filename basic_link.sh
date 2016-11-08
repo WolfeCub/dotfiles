72ef0ef14a058921d77329bf65851ebb202346fa
Josh Wolfe <joshuafwolfe@gmail.com>
2016-11-08 14:46:14 -0500

    Fixed link scripts

--- basic_link.sh
+++ basic_link.sh
@@ -4,6 +4,6 @@ ln -sd `pwd`/.vim ~
 ln -s `pwd`/.vimrc ~
 ln -sd `pwd`/.emacs.d ~
 ln -s `pwd`/.tmux.conf ~
-ln -sd `pwd`/zsh ~
-ln -s `pwd`/zshrc ~
-ln -s `pwd`/Xmodmap ~
+ln -sd `pwd`/.zsh ~
+ln -s `pwd`/.zshrc ~
+ln -s `pwd`/.Xmodmap ~
--- full_link.sh
+++ full_link.sh
@@ -11,6 +11,6 @@ ln -sd `pwd`/.vim ~
 ln -s `pwd`/.vimrc ~
 ln -sd `pwd`/.emacs.d ~
 ln -s `pwd`/.tmux.conf ~
-ln -sd `pwd`/zsh ~
-ln -s `pwd`/zshrc ~
-ln -s `pwd`/Xmodmap ~
+ln -sd `pwd`/.zsh ~
+ln -s `pwd`/.zshrc ~
+ln -s `pwd`/.Xmodmap ~

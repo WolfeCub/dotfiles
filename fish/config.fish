9dd062efa0980f60784ae87ccce603a188d44728
Josh Wolfe <joshuafwolfe@gmail.com>
2016-12-12 04:25:25 -0500

    Started using fish shell

---
+++ fish/config.fish
@@ -0,0 +1,124 @@
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings hybrid_bindings
+
+##                                                                   
+## A L I A S E S                                                     
+##                                                                   
+# Misc                                                               
+alias :q 'exit'                                                      
+alias less 'less -R'                                                 
+alias grep 'grep --color=always'                                     
+# List directory contents                                            
+alias lsa 'ls -lah --color'                                          
+alias l 'ls --color'                                                 
+alias ll 'ls -lh --color'                                            
+alias la 'ls -A --color'                                             
+alias ls 'ls --color'                                                
+# Push and pop directories on directory stack                        
+alias md 'mkdir -p'                                                  
+alias rd rmdir                                                       
+alias d 'dirs -v | head -10'                                         
+# Emacs clients                                                      
+alias ed 'emacs -nw'                                                 
+alias et 'emacsclient -t'                                            
+alias ec 'emacsclient -c'                                            
+alias magit 'ed -eval "(progn (magit-status)(delete-other-windows))"'
+
+
+function fish_prompt --description 'Write out the prompt'
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream "informative"
+    end
+    if not set -q __fish_git_prompt_char_upstream_ahead
+        set -g __fish_git_prompt_char_upstream_ahead "↑"
+    end
+    if not set -q __fish_git_prompt_char_upstream_behind
+        set -g __fish_git_prompt_char_upstream_behind "↓"
+    end
+    if not set -q __fish_git_prompt_char_upstream_prefix
+        set -g __fish_git_prompt_char_upstream_prefix ""
+    end
+
+    if not set -q __fish_git_prompt_char_stagedstate
+        set -g __fish_git_prompt_char_stagedstate "●"
+    end
+    if not set -q __fish_git_prompt_char_dirtystate
+        set -g __fish_git_prompt_char_dirtystate "✚"
+    end
+    if not set -q __fish_git_prompt_char_untrackedfiles
+        set -g __fish_git_prompt_char_untrackedfiles "…"
+    end
+    if not set -q __fish_git_prompt_char_conflictedstate
+        set -g __fish_git_prompt_char_conflictedstate "✖"
+    end
+    if not set -q __fish_git_prompt_char_cleanstate
+        set -g __fish_git_prompt_char_cleanstate "✔"
+    end
+
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
+
+    set -l last_status $status
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    set -l color_cwd
+    set -l prefix
+    switch $USER
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix '$'
+    end
+
+    # PWD
+    set_color $color_cwd
+    echo -n (echo $PWD | sed -e "s|^$HOME|~|")
+    set_color normal
+
+    printf '%s ' (__fish_vcs_prompt)
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    echo -n "$suffix "
+
+    set_color normal
+end

3f91e52e2689aea2e4b71c046154de24050a431c
Josh Wolfe <jwolfe@indigo.ca>
2018-04-13 17:00:39 -0400

    Fixed an error when no candidates where found

--- emacs/.emacs.d/lisp/doom-todo-ivy.el
+++ emacs/.emacs.d/lisp/doom-todo-ivy.el
@@ -14,23 +14,24 @@ face to render it with.")
 (defun doom/ivy--tasks-candidates (tasks)
   "Generate a list of task tags (specified by `doom/ivy-task-tags') for
 `doom/ivy-tasks'."
-  (let* ((max-type-width
-          (cl-loop for task in doom/ivy-task-tags maximize (length (car task))))
-         (max-desc-width
-          (cl-loop for task in tasks maximize (length (cl-cdadr task))))
-         (max-width (max (- (frame-width) (1+ max-type-width) max-desc-width)
-                         25)))
-    (cl-loop
-     with fmt = (format "%%-%ds %%-%ds%%s%%s:%%s" max-type-width max-width)
-     for alist in tasks
-     collect
-     (let-alist alist
-       (format fmt
-               (propertize .type 'face (cdr (assoc .type doom/ivy-task-tags)))
-               (string-trim (substring .desc 0 (min max-desc-width (length .desc))))
-               (propertize " | " 'face 'font-lock-comment-face)
-               (propertize (abbreviate-file-name .file) 'face 'font-lock-keyword-face)
-               (propertize .line 'face 'font-lock-constant-face))))))
+  (when tasks
+    (let* ((max-type-width
+            (cl-loop for task in doom/ivy-task-tags maximize (length (car task))))
+           (max-desc-width
+            (cl-loop for task in tasks maximize (length (cl-cdadr task))))
+           (max-width (max (- (frame-width) (1+ max-type-width) max-desc-width)
+                           25)))
+      (cl-loop
+       with fmt = (format "%%-%ds %%-%ds%%s%%s:%%s" max-type-width max-width)
+       for alist in tasks
+       collect
+       (let-alist alist
+         (format fmt
+                 (propertize .type 'face (cdr (assoc .type doom/ivy-task-tags)))
+                 (string-trim (substring .desc 0 (min max-desc-width (length .desc))))
+                 (propertize " | " 'face 'font-lock-comment-face)
+                 (propertize (abbreviate-file-name .file) 'face 'font-lock-keyword-face)
+                 (propertize .line 'face 'font-lock-constant-face)))))))
 
 (defun doom/ivy--tasks (target)
   (let* (case-fold-search

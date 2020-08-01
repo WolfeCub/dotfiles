0f0fb1d204bb7bde406033afc47b5e88b9e20229
Josh Wolfe <jfwolfe@spscommerce.com>
2020-08-01 17:20:08 -0400

    Cleaned up emacs config and fixed git aliases

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -15,7 +15,7 @@
       ad-redefinition-action 'accept)
 
 (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold 16777216
+          (lambda () (setq gc-cons-threshold 100000000
                       gc-cons-percentage 0.1
                       file-name-handler-alist wolfe/file-name-handler-alist
                       ad-redefinition-action 'warn)))

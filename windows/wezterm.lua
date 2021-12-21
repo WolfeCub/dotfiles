8f3a1d82b2652f7c3bb8a6122d9ae2db93e9747b
Josh Wolfe <joshuafwolfe@gmail.com>
2021-12-21 12:10:27 -0500

    Tweak wezterm behaviour

--- windows/wezterm.lua
+++ windows/wezterm.lua
@@ -1,19 +1,41 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("Fira Code"),
-  color_scheme = "Builtin Dark",
-  exit_behavior = "Close",
+    font = wezterm.font("FiraCode NF"),
+    color_scheme = "Builtin Dark",
+    window_decorations = "RESIZE",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 
-  default_prog = {"wsl", "-u", "wolfe"},
+    exit_behavior = "Close",
+    default_prog = {"wsl", "-u", "wolfe"},
+    default_cwd = "\\\\wsl$\\Arch\\home\\wolfe",
 
-  leader = { key="h", mods="CTRL", timeout_milliseconds=1000 },
-  keys = {
-      {key="5", mods="LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-      {key="\"", mods="LEADER|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-      {key="h", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
-      {key="l", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
-      {key="k", mods="ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
-      {key="j", mods="ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
-  }
+    leader = { key="h", mods="CTRL", timeout_milliseconds=1000 },
+    keys = {
+        -- New tab launcher
+        {key="t", mods="CTRL|ALT", action="ShowLauncher"},
+
+        -- Tmux like splitting
+        {key="5", mods="LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        {key="\"", mods="LEADER|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="h", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
+        {key="l", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="k", mods="ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
+        {key="j", mods="ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
+
+        -- Clear
+        {key="^", mods="CTRL", action="DisableDefaultAssignment"},
+    },
+
+    launch_menu = {
+        { 
+            label = "Git Bash",
+            args = {"C:\\Program Files\\Git\\bin\\bash.exe"},
+        },
+    }
 }

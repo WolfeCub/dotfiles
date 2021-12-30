6240b9fc883bf4bbfb69187153de4d90d70f721f
Josh Wolfe <joshuafwolfe@gmail.com>
2021-12-30 15:56:46 -0500

    Refactor lsp neovim config

--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,9 +1,46 @@
-local lsp = require('lspconfig')
+local nvim_lsp = require('lspconfig')
 
-lsp.tsserver.setup(coq.lsp_ensure_capabilities({}))
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
-lsp.omnisharp.setup{
-    cmd = { "omnisharp", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) };
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+
+    local opts = { noremap=true, silent=true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 
+    tsserver = {},
+    omnisharp = {
+        cmd = { "omnisharp", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) },
+    },
+    graphl = {},
 }
 
-lsp.graphql.setup{}
+for lsp, config in ipairs(servers) do
+    local options = {
+        on_attach = on_attach,
+    }
+    for k, v in pairs(config) do 
+        options[k] = v
+    end
+    nvim_lsp[lsp].setup(coq.lsp_ensure_capabilities(options))
+end

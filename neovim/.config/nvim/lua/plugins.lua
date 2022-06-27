16c5a19d558a1e2d7c04f8ee34ca1dbde0bb5999
Josh Wolfe <joshuafwolfe@gmail.com>
2022-06-27 11:04:37 -0400

    Add treesitter

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -19,6 +19,21 @@ return require('packer').startup(function()
     use 'ggandor/lightspeed.nvim'
 
     use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                sync_install = false,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            });
+            require('plugins.treesitter-config');
+        end
+    }
+
+    use {
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true},
         config = function() require('plugins.lualine-config') end
---
+++ neovim/.config/nvim/lua/plugins/treesitter-config.lua
@@ -0,0 +1,18 @@
+local ask_install = {}
+function _G.ensure_treesitter_language_installed()
+  local parsers = require "nvim-treesitter.parsers"
+  local lang = parsers.get_buf_lang()
+  if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) and ask_install[lang] ~= false then
+    vim.schedule_wrap(function()
+      vim.ui.select({"yes", "no"}, { prompt = "Install tree-sitter parsers for " .. lang .. "?" }, function(item)
+        if item == "yes" then
+          vim.cmd("TSInstall " .. lang)
+        elseif item == "no" then
+          ask_install[lang] = false
+        end
+      end)
+    end)()
+  end
+end
+
+vim.cmd [[autocmd FileType * :lua ensure_treesitter_language_installed()]]

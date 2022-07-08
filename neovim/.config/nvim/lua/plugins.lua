c2c58b4dbf2d08ac201329815ce36660f4a00cf0
Josh Wolfe <joshuafwolfe@gmail.com>
2022-07-08 10:31:21 -0400

    Tweak theme settings

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -12,7 +12,9 @@ return require('packer').startup(function()
     use {
         'folke/tokyonight.nvim',
         config = function()
-            vim.g.tokyonight_style = 'night'
+            vim.g.tokyonight_style = 'night';
+            vim.g.tokyonight_italic_comments = false;
+            vim.g.tokyonight_italic_keywords = false;
             vim.cmd('colorscheme tokyonight');
         end
     }

45ae36df5b0dec1961bd1737b3fdda95871675c7
Josh Wolfe <wolfe@dropbox.com>
2024-02-06 13:45:35 -0500

    Final install tweaks

--- install.sh
+++ install.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 REPOPATH="$(find ~ -name dotfiles -type d)"
-echo "Found dotfiles at: $REPOPATH"
 
 if [ -z "$REPOPATH" ]; then
     echo "Could not find dotfiles repository"
     exit 1
+else
+    echo "Found dotfiles at: $REPOPATH"
 fi
 
 NVIMPATH="$HOME/.config/nvim"
 if [ -d "$NVIMPATH" ]; then
     cd $NVIMPATH
+    echo "Found nvim config at: $NVIMPATH. Updating..."
     git pull
 else
+    echo "Cloning nvim config to: $NVIMPATH"
     git clone https://github.com/Mulan-Szechuan-Sauce/nvim-config $NVIMPATH
 fi
 
 cd $REPOPATH
 
+echo "Initializing and updating submodules"
 git submodule init
 git submodule update
 
-ln -s $REPOPATH/zsh/.zshrc $HOME
-ln -s $REPOPATH/zsh/.zsh $HOME
+echo "Linking zsh"
+ln -f -s $REPOPATH/zsh/.zshrc $HOME
+ln -f -s $REPOPATH/zsh/.zsh $HOME
 
-ln -s $REPOPATH/neovim/.user.nvim $HOME
+echo "Linking user nvim"
+ln -f -s $REPOPATH/neovim/.user.nvim $HOME
 
-ln -s $REPOPATH/tmux/.tmux.conf $HOME
+echo "Linking tmux"
+ln -f -s $REPOPATH/tmux/.tmux.conf $HOME
